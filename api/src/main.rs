badec0022ccb6ce7a848c00fa06b377ba120f6af
Bijoy99roy <bijoy2014amp@gmail.com>
2025-06-22 15:03:52 +0000

    feat: Implement multithread object handling

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,26 +1,70 @@
+use std::sync::{Arc, Mutex};
+
 use poem::{
     get, handler,
     listener::TcpListener,
     post,
-    web::{Json, Path},
-    Route, Server,
+    web::{Data, Json, Path},
+    EndpointExt, Route, Server,
 };
 use store::store::Store;
 
-use crate::{request_inputs::CreateWebsiteInput, request_outputs::CreateWebsiteOutput};
+use crate::{
+    request_inputs::{CreateUserInput, CreateWebsiteInput},
+    request_outputs::{CreateUserOutput, CreateWebsiteOutput, GetWebsiteOutput, SigninOutput},
+};
 
 pub mod request_inputs;
 pub mod request_outputs;
 
 #[handler]
-fn get_website(Path(name): Path<String>) -> String {
-    format!("hello: {name}")
+fn sign_up(
+    Json(data): Json<CreateUserInput>,
+    Data(store): Data<&Arc<Mutex<Store>>>,
+) -> Json<CreateUserOutput> {
+    let mut locked_store = store.lock().unwrap();
+    let user: String = locked_store.sign_up(data.username, data.password).unwrap();
+
+    let response = CreateUserOutput { id: user };
+
+    Json(response)
 }
 
 #[handler]
-fn create_websites(Json(data): Json<CreateWebsiteInput>) -> Json<CreateWebsiteOutput> {
-    let mut store = Store::default().unwrap();
-    let website = store
+fn sign_in(
+    Json(data): Json<CreateUserInput>,
+    Data(store): Data<&Arc<Mutex<Store>>>,
+) -> Json<SigninOutput> {
+    let mut locked_store = store.lock().unwrap();
+    let exists = locked_store.sign_in(data.username, data.password).unwrap();
+
+    let response = SigninOutput {
+        jwt: "Hello".to_string(),
+    };
+
+    Json(response)
+}
+
+#[handler]
+fn get_website(
+    Path(id): Path<String>,
+    Data(store): Data<&Arc<Mutex<Store>>>,
+) -> Json<GetWebsiteOutput> {
+    let mut locked_store = store.lock().unwrap();
+    let website = locked_store.get_website(id).unwrap();
+
+    let response = GetWebsiteOutput { url: website.url };
+
+    Json(response)
+}
+
+#[handler]
+fn create_websites(
+    Json(data): Json<CreateWebsiteInput>,
+    Data(store): Data<&Arc<Mutex<Store>>>,
+) -> Json<CreateWebsiteOutput> {
+    let mut locked_store = store.lock().unwrap();
+    let website = locked_store
         .create_website("6e79c3aa-7b44-48a1-bdad-09a828efd76b".to_string(), data.url)
         .unwrap();
 
@@ -28,11 +72,15 @@ fn create_websites(Json(data): Json<CreateWebsiteInput>) -> Json<CreateWebsiteOu
 
     Json(response)
 }
-#[tokio::main]
+#[tokio::main(flavor = "multi_thread")]
 async fn main() -> Result<(), std::io::Error> {
+    let arced_store = Arc::new(Mutex::new(Store::default().unwrap()));
     let app = Route::new()
         .at("/status/:website_id", get(get_website))
-        .at("/website", post(create_websites));
+        .at("/website", post(create_websites))
+        .at("/user/signup", post(sign_up))
+        .at("/user/signin", post(sign_in))
+        .data(arced_store);
     Server::new(TcpListener::bind("0.0.0.0:3000"))
         .name("tracker")
         .run(app)
--- store/src/config.rs
+++ store/src/config.rs
@@ -1,14 +1,17 @@
 use std::env;
 
+use dotenvy::dotenv;
+
 pub struct Config {
     pub database_url: String,
 }
 
 impl Default for Config {
     fn default() -> Self {
+        dotenv().ok();
         let database_url = env::var("DATABASE_URL")
             .unwrap_or_else(|_| panic!("PLease provide the DATABASE_URL environment variable"));
-
+        print!("DB URL: {}", database_url);
         Self { database_url }
     }
 }
